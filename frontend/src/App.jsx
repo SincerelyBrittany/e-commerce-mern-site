855b198471135109d45c41ae8eaba53725d8df1a
Sincerely Brittany <sincerelybr@gmail.com>
2023-04-30 17:58:47 -0400

    adds cart page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,13 +10,15 @@ import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
     <div>
       <Home />
-      <Login />
-      <Register />
+      <Cart />
+      {/* <Login />
+      <Register /> */}
     </div>
   );
 };
