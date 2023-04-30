b708bfb792234f089f7c0390f0fd83637aeae070
Sincerely Brittany <sincerelybr@gmail.com>
2023-04-30 14:48:33 -0400

    adds subscription section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Announcement from "./components/Announcement";
 import Categories from "./components/Categories";
 import Navbar from "./components/Navbar";
+import Subscription from "./components/Subscription";
 import Products from "./components/Products";
 import Slider from "./components/Slider";
 import { Home } from "./pages/Home";
@@ -10,9 +11,10 @@ const App = () => {
     <div>
       <Announcement />
       <Navbar />
-      {/* <Slider />
-      <Categories /> */}
+      <Slider />
+      <Categories />
       <Products />
+      <Subscription />
     </div>
   );
 };
