c9222100b3f630b3a294d40106a6f34bb307084a
Sincerely Brittany <sincerelybr@gmail.com>
2023-04-30 17:41:53 -0400

    adds login and sign up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,19 @@ import Navbar from "./components/Navbar";
 import Subscription from "./components/Subscription";
 import Products from "./components/Products";
 import Slider from "./components/Slider";
-import { Home } from "./pages/Home";
+import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div>
-      {/* <Home /> */}
-      <Product />
+      <Home />
+      <Login />
+      <Register />
     </div>
   );
 };
