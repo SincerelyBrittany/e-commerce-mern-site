16b0f925be4fce33a91f7a41fc0311834852c3de
Sincerely Brittany <sincerelybr@gmail.com>
2023-05-15 22:08:35 -0400

    adds routes

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,22 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const userRoute = require("./routes/user");
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DBConnection sucessful"))
   .catch((err) => {
     console.log(err);
   });
+
+app.get("/api/test", () => {
+  console.log("test is sucessful");
+});
+
+app.use("/api/user", userRoute); //5000/api/user/usertest
+
+// app.listen(process.env.PORT || 5001, () => {
 
 app.listen(5001, () => {
   console.log("Backend Is Running ");
